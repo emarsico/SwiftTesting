abc28c3d22321419e762fd0d652efc9eb9cb0acd
emarsico <elliotmarsico@outlook.com>
2024-04-28 15:19:20 -0400

    Init

---
+++ AudioTesting/AudioTesting/AudioTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTestingApp.swift
+//  AudioTesting
+//
+//  Created by Vision Solutions on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
